930ac35bd9c0ff3a206a2f503ff35e4eb5c0d7cc
ejsado <esadowski@gmail.com>
2016-07-13 15:07:31 -0400

    added note to about modal

--- public/js/alertFty.js
+++ public/js/alertFty.js
@@ -111,7 +111,8 @@ function alertFty($sce, $timeout) {
 					text: [
 						'<a href="https://developer.forecast.io/">Dark Sky</a> provides the weather data',
 						'<a href="https://developers.google.com/maps/">Google</a> provides the map and location data',
-						'<a href="https://www.wunderground.com/?apiref=98798f3caba1662f">Weather Underground</a> provides the precipitation radar which is overlayed on the map'
+						'<a href="https://www.wunderground.com/?apiref=98798f3caba1662f">Weather Underground</a> provides the precipitation radar which is overlayed on the map',
+						'* Note that radar is only available for North America, Europe, and Australia.'
 					]
 				},
 				{
